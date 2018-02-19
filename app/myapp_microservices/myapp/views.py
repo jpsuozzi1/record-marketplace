971a14c584d886dd605d21fb4aa9a3da3c431763
Julia Suozzi <juliasuozzi@d-172-25-98-68.dhcp.virginia.edu>
2018-02-19 15:54:31 -0500

    add route for home

--- app/myapp_microservices/myapp/views.py
+++ app/myapp_microservices/myapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
+def home(request):
+	return HttpResponse("Welcome to the home page!")
 
 
 def create(request, model):
