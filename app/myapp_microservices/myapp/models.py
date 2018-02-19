21c0e3929b5629401fd12b71dbb76219300b0019
Josh G <jmg6kw@virginia.edu>
2018-02-18 22:56:41 -0500

    Created Initial Models

--- app/myapp_microservices/myapp/models.py
+++ app/myapp_microservices/myapp/models.py
@@ -1,21 +1,44 @@
 from django.db import models
 
+
+# See https://docs.djangoproject.com/en/1.10/ref/models/fields/#django.db.models.ForeignKey
+
 # Users
 #     Name
 #     Email
-#     Password or something (Hash of it?)
+#     Password Hash
 #     Sell lists
 #     Purchase History
 class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    # https://docs.djangoproject.com/en/1.10/topics/auth/passwords/
+    passwordHash = models.CharField(max_length=100)
+
+    @property
+    def sellList(self):
+        return self.listing_set.filter(listing__name='seller')
+
+    @property
+    def purchaseHistory(self):
+        return self.listing_set.filter(listing__name='buyer')
+
 
 
 # Artist
 class Artist(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    
+
+    @property
+    def songList(self):
+        return self.song_set.all()
+
+    @property
+    def albumList(self):
+        return self.artist_set.all()
 
 
 # Records
@@ -26,28 +49,33 @@ class Artist(models.Model):
 #     Length 
 #     Edition/Pressing
 #     List of genres
-
 class Record(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
-    #length = models.DurationField()
+    pressing = models.CharField(max_length=100);
 
     @property
     def length(self): # Duration of entire album
-        songs = self.song_set.all()
-        return sum(song.duration for song in songs)
+        songSet = self.song_set.all()
+        return sum(song.duration for song in songSet)
+    
+    @property
+    def songLists(self):
+        return self.song_set.all()
         
 
 # Songs 
 #     Duration
 #     Name
-#     Artists
+#     Artist
+#     Record
 class Song(models.Model):
     name = models.CharField(max_length=100)
     duration = models.DurationField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     record = models.ForeignKey(Record, on_delete=models.CASCADE)
+
 
 # Listing
 #     Price
@@ -55,5 +83,44 @@ class Song(models.Model):
 #     Record
 #     Condition
 #     Date Posted
+class Listing(models.Model):
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    seller = models.ForeignKey(User, related_name='seller', on_delete=models.CASCADE)
+    buyer = models.ForeignKey(User, related_name='buyer', on_delete=models.CASCADE)
+    record = models.ForeignKey(Record, on_delete=models.CASCADE)
+    date_posted = models.DateField()
+    
+    # Condition Options: Mint, Near Mint, Very Good, Good, Fair, Poor 
+    MINT = 'M'
+    NEARMINT = 'NM'
+    VERYGOOD = 'VG'
+    GOOD = 'G'
+    FAIR = 'F'
+    POOR = 'P'
 
+    CONDITION_CHOICES = (
+        (MINT, 'Mint'),
+        (NEARMINT, 'Near Mint'),
+        (VERYGOOD, 'Very Good'),
+        (GOOD, 'Good'),
+        (FAIR, 'Fair'),
+        (POOR, 'Poor')
+    )
 
+    condition = models.CharField(
+        max_length=2,
+        choices=CONDITION_CHOICES,
+        default=POOR,
+    )
+
+# Genre
+# Name
+# Record
+class Genre(models.Model):
+    record = models.ForeignKey(
+        Record,
+        on_delete=models.CASCADE,
+        related_name="genre",
+        related_query_name="genre",
+    )
+    name = models.CharField(max_length=100)
